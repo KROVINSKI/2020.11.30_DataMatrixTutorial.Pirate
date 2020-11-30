7b9ecd9719e406c5d71f6c94e7e41c44099f5cf7
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-11-30 12:36:31 -0800

    2020.11.30 sections 8.1c down at line 90

--- 2020.11.30_Chapter8MatricesandDataframes.R
+++ 2020.11.30_Chapter8MatricesandDataframes.R
@@ -12,6 +12,11 @@
 # Basic dataframe operations
 # -----------------------------
 
+
+# -----------------------------
+# 8.1a Creating the Data Frame
+# -----------------------------
+
 # Create a dataframe of boat sale data called bsale
 bsale <- data.frame(name = c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j"),
                     color = c("black", "green", "pink", "blue", "blue", 
@@ -21,12 +26,47 @@ bsale <- data.frame(name = c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j"),
                     cost = c(52, 80, 20, 100, 189, 12, 520, 68, 80, 100),
                     stringsAsFactors = FALSE)   # Don't convert strings to factors!
 
+
+# -----------------------------
+# 8.1b Exploring dataset aspects
+# -----------------------------
+
 # Explore the bsale dataset:
+
+## head function
+# Return the First or Last Part of an Object
 head(bsale)     # Show me the first few rows
+
+
+## 'Structure' dataframe utility function
+# Compactly Display the Structure of an Arbitrary R Object
+# gives a small preview simular to the environment window inside R studio
 str(bsale)      # Show me the structure of the data
+
+
+## View Tool
+# Invoke a Data Viewer
+# Spreadsheet style data viewer on a matrix like r object
 View(bsale)     # Open the data in a new window
+
+
+## Names of an object function
+# straight forward tool 
+# names (x) would be the r object
+# the names function itself displays a character vector of up to the same length as x, or null
 names(bsale)    # What are the names of the columns?
+# [1] "name"        "color"       "age"         "price"       "cost"        "id"          "age.decades" "profit"  
+
+
+## The Number of Rows/Columns of an Array 
 nrow(bsale)     # How many rows are there in the data?
+# [1] 10
+
+
+
+# -----------------------------
+# 8.1c Statistics with Column Vectors
+# -----------------------------
 
 # Calculating statistics from column vectors
 mean(bsale$age)       # What was the mean age?
