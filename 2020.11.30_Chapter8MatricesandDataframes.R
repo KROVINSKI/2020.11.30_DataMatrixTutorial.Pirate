2fc4e230f7fd6b282421fd3c864468fdab25bef2
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-11-30 12:53:29 -0800

    2020.11.30 section 8.1c complete up to line 123

--- 2020.11.30_Chapter8MatricesandDataframes.R
+++ 2020.11.30_Chapter8MatricesandDataframes.R
@@ -67,31 +67,59 @@ nrow(bsale)     # How many rows are there in the data?
 # -----------------------------
 # 8.1c Statistics with Column Vectors
 # -----------------------------
-
 # Calculating statistics from column vectors
+
+
 mean(bsale$age)       # What was the mean age?
 table(bsale$color)    # How many boats were there of each color?
 max(bsale$price)      # What was the maximum price?
+## Results QA check
+# > mean(bsale$age)       # What was the mean age?
+# [1] 197.3
+# > table(bsale$color)    # How many boats were there of each color?
+# 
+# black   blue  green   pink yellow 
+# 3      2      3      1      1 
+# > max(bsale$price)      # What was the maximum price?
+# [1] 532
+
 
 # Adding new columns
+# Creating ID, ages inside decades, and profit
 bsale$id <- 1:nrow(bsale)
 bsale$age.decades <- bsale$age / 10
 bsale$profit <- bsale$price - bsale$cost
 
+
+### 8.1.c.1 Evaluate an Expression in a Data Environment 
+# Evaluate an R expression in an environment constructed from data, possibly modifying (making a copy of) the original data. 
+
 # What was the mean price of green boats?
 with(bsale, mean(price[color == "green"]))
+# [1] 217
 
 # What were the names of boats older than 100 years?
 with(bsale, name[age > 100])
+# [1] "a" "c" "e" "g"
+
 
 # What percent of black boats had a positive profit?
 with(subset(bsale, color == "black"), mean(profit > 0))
+# [1] 1
+
 
 # Save only the price and cost columns in a new dataframe
 bsale.2 <- bsale[c("price", "cost")]
+# 10 observations
+# 2 variables
+
 
 # Change the names of the columns to "p" and "c"
 names(bsale.2) <- c("p", "c")
 
 # Create a dataframe called old.black.bsale containing only data from black boats older than 50 years
 old.black.bsale <- subset(bsale, color == "black" & age > 50)
+# 3 observations
+# 8 variables
+
+
